f22a329531484eb1adbca46cfbfe617c48af340c
Makeademic <kaplanwizard@gmail.com>
2025-06-23 13:14:45 +0000

    Added CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    files: './public/static/**/*.css',
+  });
+
   return {
     dir: {
-      input: "src",
-      output: "public",
+      input: 'src',
+      output: 'public',
     },
   };
-};
+};  
